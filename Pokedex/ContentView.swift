36c825ad7dce1d6bafc6c7c5e66bd4ccb7c242e5
cloviscarmezini <cloviscarmezini14@gmail.com>
2024-07-02 22:26:14 -0300

    added persistence and favorite feature

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -9,31 +9,45 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
-        animation: .default)
-    private var pokedex: FetchedResults<Pokemon>
+        animation: .default
+    ) private var pokedex: FetchedResults<Pokemon>
     
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
+        predicate: NSPredicate(format: "favorite = %d", true),
+        animation: .default
+    ) private var favorites: FetchedResults<Pokemon>
+    
+    @State var filterByFavorites = false
     @StateObject private var prokemonVM = PokemonViewModel(controller: FetchController())
 
     var body: some View {
         switch prokemonVM.status {
             case .success:
                 NavigationStack {
-                    List(pokedex) { pokemon in
+                    List(filterByFavorites ? favorites : pokedex) { pokemon in
                         NavigationLink(value: pokemon) {
-                            AsyncImage(url: pokemon.sprite) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                            } placeholder: {
-                                ProgressView()
+                            HStack(alignment: .center) {
+                                AsyncImage(url: pokemon.sprite) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: 100, height: 100)
+                                
+                                Text(pokemon.name!.capitalized)
+                                
+                                Spacer()
+                                
+                                if pokemon.favorite {
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                }
                             }
-                            .frame(width: 100, height: 100)
-                            
-                            Text(pokemon.name!.capitalized)
                         }
                     }
                     .navigationTitle("Pokedex")
@@ -43,7 +57,15 @@ struct ContentView: View {
                     })
                     .toolbar {
                         ToolbarItem(placement: .navigationBarTrailing) {
-                            EditButton()
+                            Button {
+                                withAnimation {
+                                    filterByFavorites.toggle()
+                                }
+                            } label: {
+                                Image(systemName: filterByFavorites ? "star.fill" : "star")
+                            }
+                            .font(.title)
+                            .foregroundColor(.yellow)
                         }
                     }
                 }
